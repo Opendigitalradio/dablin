8f91a1e0173777cc3eb214f58327cd793d590a16
Stefan Pöschel <github@basicmaster.de>
2024-04-07 20:37:51 +0200

    MP2: re-enable libmpg123 large file API
    While in the past Cygwin needed it disabled, it now also works when
    enabled. Fixes compilation on armel/armhf with libmpg123 v1.32.6.

--- src/dab_decoder.h
+++ src/dab_decoder.h
@@ -26,7 +26,6 @@
 #include <string>
 #include <vector>
 
-#define MPG123_NO_LARGENAME	// disable large file API here
 #include "mpg123.h"
 #if MPG123_API_VERSION < 36
 #	error "At least version 1.14.0 (API version 36) of mpg123 is required!"
